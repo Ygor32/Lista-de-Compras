7e52baa9daa236a2da52b0983edb4efe8da1551c
Ygor Botelho Mota <ygor.b.32@gmail.com>
2024-11-25 20:42:37 -0300

    Delete construindoPaginasDinamicas directory

--- construindoPaginasDinamicas/js/gerarDiaDaSemana.js
+++
@@ -1,3 +0,0 @@
-export function gerarDiaDaSemana() {
-    return `${new Date().toLocaleDateString("pt-BR", { weekday: "long" })} (${new Date().toLocaleDateString()}) às ${new Date().toLocaleTimeString("pt-BR", { hour: "numeric", minute: "numeric" })}`
-}
